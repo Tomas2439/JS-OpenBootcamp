413cef9a94460b655de6e9164c7796db038a2fc9
Tomas Stutz <tomasbautistastutz@gmail.com>
2023-05-23 23:36:13 -0300

    Ejercicio 4

---
+++ ejercicio-4/ejercicio4.js
@@ -0,0 +1,15 @@
+//Cadenas de texto
+let nombre = 'Tomás Bautista';
+
+let apellido = 'Stutz';
+
+let estudiante = nombre + ' '+ apellido;
+
+console.log('Estudiante: ',estudiante);
+
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+
+console.log('Mayúsculas: ',estudianteMayus);
+console.log('Minúsculas: ', estudianteMinus);
+
